c661907d6b9a08fbaa1d20a5193763b874e88ad6
Bartek Woldański <bart@dev-1.pl>
2021-11-08 21:50:36 +0100

    drawing on canvas works fine

--- src/components/Draw.tsx
+++ src/components/Draw.tsx
@@ -1,12 +1,9 @@
-import React, {
-  FunctionComponent,
-  MouseEventHandler,
-  useState,
-} from "react";
+import React, { FunctionComponent, MouseEventHandler, useState } from "react";
 import Canvas from "./Canvas";
 
 const Draw: FunctionComponent = () => {
   const [canvasSize, setCanvasSize] = useState({ width: 500, height: 500 });
+  const [drawOn, setDrawOn] = useState(false);
   const [mouseDownCoordinates, setMouseDownCoordinates] = useState({
     x: 0,
     y: 0,
@@ -17,18 +14,31 @@ const Draw: FunctionComponent = () => {
   });
 
   const draw = (ctx: CanvasRenderingContext2D) => {
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 10;
-    ctx.moveTo(mouseDownCoordinates.x-ctx.canvas.clientLeft, mouseDownCoordinates.y-ctx.canvas.clientTop);
-    ctx.lineTo(mouseMoveCoordinates.x, mouseMoveCoordinates.y);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'round';
+    ctx.beginPath();
+    if (drawOn) {
+      ctx.moveTo(mouseDownCoordinates.x, mouseDownCoordinates.y);
+      ctx.lineTo(
+        mouseMoveCoordinates.x - ctx.canvas.offsetLeft,
+        mouseMoveCoordinates.y - ctx.canvas.offsetTop
+      );
+      setMouseDownCoordinates(mouseMoveCoordinates);
+      ctx.stroke();
+    }
   };
 
-    const handleCanvasMouse: MouseEventHandler<HTMLCanvasElement> = (e) => {
-      
-    if (e.type === "mousedown")
+  const handleCanvasMouse: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    if (e.type === "mousedown") {
+      setDrawOn(true);
       setMouseDownCoordinates({ x: e.clientX, y: e.clientY });
+    }
 
-    if (e.type === "mouseup") setMouseDownCoordinates({ x: 0, y: 0 });
+    if (e.type === "mouseup") {
+      setDrawOn(false);
+      setMouseDownCoordinates({ x: 0, y: 0 });
+    }
 
     if (e.type === "mousemove")
       setMouseMoveCoordinates({ x: e.pageX, y: e.pageY });
